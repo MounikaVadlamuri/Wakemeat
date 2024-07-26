ee831980e8782488da97d58d053fd2d58ad0072f
Cesar Santos <cesar.santos@loadsmart.com>
2024-07-26 09:09:25 -0400

    small changes in UI + delete method

--- Wakemeat/app/src/main/java/com/capstone/wakemeat/DatabaseHelper.java
+++ Wakemeat/app/src/main/java/com/capstone/wakemeat/DatabaseHelper.java
@@ -10,10 +10,17 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "wake.db";
+    /* USERS_USER */
     public static final String TABLE_NAME_USER = "users_user";
     public static final String TABLE_COLUMN_USER_ID = "id";
     public static final String TABLE_COLUMN_USER_EMAIL = "email";
     public static final String TABLE_COLUMN_USER_PASSWORD = "password";
+    /* ALARMS_ALARM */
+    public static final String TABLE_NAME_ALARM = "alarms_alarm";
+    public static final String TABLE_COLUMN_ALARM_ID = "id";
+    public static final String TABLE_COLUMN_ALARM_TIME = "time";
+    public static final String TABLE_COLUMN_ALARM_LOCATION_LATITUDE = "location_latitude";
+    public static final String TABLE_COLUMN_ALARM_LOCATION_LONGITUDE = "location_longitude";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -22,12 +29,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+ TABLE_NAME_USER + " (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT)");
+        db.execSQL("CREATE TABLE "+ TABLE_NAME_USER + " ("+TABLE_COLUMN_USER_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+TABLE_COLUMN_USER_EMAIL+" TEXT, "+TABLE_COLUMN_USER_PASSWORD+" TEXT)");
+        db.execSQL("CREATE TABLE "+ TABLE_NAME_ALARM + " ("+TABLE_COLUMN_ALARM_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+TABLE_COLUMN_ALARM_TIME+" TEXT, "+TABLE_COLUMN_ALARM_LOCATION_LATITUDE+" DOUBLE, "+TABLE_COLUMN_ALARM_LOCATION_LONGITUDE+" DOUBLE)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME_USER);
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME_ALARM);
         this.onCreate(db);
     }
 
@@ -46,5 +55,34 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = db.rawQuery("SELECT ID FROM "+ TABLE_NAME_USER+ " WHERE email='" + email + "' AND password='" + password + "' LIMIT 1", null);
         return res;
+    }
+
+    public boolean insertAlarm(String time, Double latitude, Double longitude){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TABLE_COLUMN_ALARM_TIME, time);
+        contentValues.put(TABLE_COLUMN_ALARM_LOCATION_LATITUDE, latitude);
+        contentValues.put(TABLE_COLUMN_ALARM_LOCATION_LONGITUDE, longitude);
+        long result = db.insert(TABLE_NAME_ALARM, null, contentValues);
+
+        if(result == -1) return false;
+        else return true;
+    }
+
+    public Cursor listAlarmbyId(Integer alarmId){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM "+ TABLE_NAME_ALARM+ " WHERE "+TABLE_COLUMN_ALARM_ID+"="+ alarmId, null);
+        return res;
+    }
+
+    public Cursor listAllAlarms(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("SELECT * FROM "+ TABLE_NAME_ALARM, null);
+        return res;
+    }
+
+    public Integer deleteAlarmbyId(Integer alarmId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME_ALARM, TABLE_COLUMN_ALARM_ID+"="+alarmId, null);
     }
 }
